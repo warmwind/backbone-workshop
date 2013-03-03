8d4a3b77275724789d4b8bf6ba887d79e938bc8d
Peng Jiang <pengj0520@gmail.com>
2013-03-04 01:25:41 +0800

    env ready for use backbone from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneWorkshop::Application.config.secret_token = 'd7121d9ac56790a7038161c3f607b02fd6a6fd29cd7e40d5e245ea6c38804c9fcedc5c4a1fba605ba78c7551a6ba3ec1f3a2840de47676991486026409d165f1'
